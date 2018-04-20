3b476465bae6e9f20fef89aa463e46c7e6bc16de
cbasil <cbasil@126.com>
2018-04-20 16:01:10 +0800

    User can upload topic image

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use Auth;
+use App\Handlers\ImageUploadHandler;
 
 class TopicsController extends Controller
 {
@@ -62,6 +63,24 @@ class TopicsController extends Controller
 
 		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
 	}
+
+	public function upload_image(Request $request,ImageUploadHandler $uploader)
+    {
+        $data=[
+            'success' => false,
+            'msg' => '文件上传失败',
+            'file_path' => '',
+        ];
+        if($file = $request->upload_file){
+            $result=$uploader->save($request->upload_file,'topics',\Auth::id(),1024);
+            if ($result){
+                $data['file_path']=$result['path'];
+                $data['msg']='上传成功';
+                $data['success']=true;
+            }
+        }
+        return $data;
+    }
 
 
 }
